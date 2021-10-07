e8053c6310d11cf0573e579fe573520391387f4b
wenjie <wenjie.cai.6@gmail.com>
2021-10-07 20:22:47 +0800

    Question 1

---
+++ Animals/src/test/java/kingdom/SolutionTest.java
@@ -0,0 +1,11 @@
+package kingdom;
+
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void testBirdSing(){
+        Bird bird = new Bird();
+        bird.sing();
+    }
+}
